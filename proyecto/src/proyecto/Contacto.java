26b57d0b52416ba09a46da02d57583c9ee790a92
ainho <ainho@10.164.58.103>
2022-10-27 11:27:29 +0200

    añadir ventana contactos

---
+++ proyecto/src/proyecto/Contacto.java
@@ -0,0 +1,36 @@
+package proyecto;
+
+public class Contacto {
+	private String nombre;
+	private String mail;
+	private String telf;
+	private boolean favorito;
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getTelf() {
+		return telf;
+	}
+	public void setTelf(String telf) {
+		this.telf = telf;
+	}
+	public boolean isFavorito() {
+		return favorito;
+	}
+	public void setFavorito(boolean favorito) {
+		this.favorito = favorito;
+	}
+	@Override
+	public String toString() {
+		return nombre + ", " + mail + ", " + telf + ", " + favorito;
+	}
+}
