9d930ae809ab0d684e5809e8feef35467093295d
ainho <ainho@10.164.58.103>
2022-10-27 11:19:45 +0200

    corregir carpeta de proyecto

---
+++ proyecto/src/proyecto/BaseDatos.java
@@ -0,0 +1,120 @@
+package proyecto;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BaseDatos {
+
+		/**
+		 * Método que realiza la conexión con la base de datos
+		 * @param nombreBD : Nombre de la base de datos a la que nos vamos a conectar
+		 * @return Devuelve la conexión a la base de datos
+		 */
+		public static Connection initBD(String nombreBD) {
+			Connection con = null;
+			try {
+				Class.forName("org.sqlite.JDBC");
+				con = DriverManager.getConnection("jdbc:sqlite:"+nombreBD);
+						
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return con;
+		}
+		
+		public static void closeBD(Connection con) {
+			if(con!=null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		public static void crearTablas(Connection con) {
+			String sql = "CREATE TABLE IF NOT EXISTS Usuario (nombre String, apellido String, mail String, nomUsuario String, contrasenia String)";
+			try {
+				Statement st = con.createStatement();
+				st.executeUpdate(sql);
+				st.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		public static void insertarUsuario(Connection con, String nombre, String apellido, String mail, String nomUsuario, String contrasenia) {
+			String sql = "INSERT INTO Usuario VALUES('"+nombre+"','"+apellido+"','"+mail+"','"+nomUsuario+"','"+contrasenia+"')";
+			try {
+				Statement st = con.createStatement();
+				st.executeUpdate(sql);
+				st.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		public static boolean buscarUsuario(Connection con, String nomUsuario) {
+			String sql = "SELECT * FROM Usuario WHERE nomUsuario='"+nomUsuario+"'";
+			boolean usuarioEncontrado = false;
+			try {
+				Statement st = con.createStatement();
+				ResultSet rs = st.executeQuery(sql); //no deja crear usuarios
+				if(rs.next()) {
+					usuarioEncontrado = true;
+				}
+				rs.close();
+				st.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return usuarioEncontrado;
+		}
+		
+		public static Usuario obtenerDatosUsuario (Connection con, String nomUsuario) {
+			String sql = "SELECT * FROM Usuario WHERE nomUsuario='"+nomUsuario+"'";
+			Usuario u = null;
+			try {
+				Statement st = con.createStatement();
+				ResultSet rs = st.executeQuery(sql);
+				if(rs.next()) {
+					String n = rs.getString("nombre");
+					String a = rs.getString("apellido");
+					String m = rs.getString("mail");
+					String nu = rs.getString("nomUsuario");
+					String c = rs.getString("contrasenia");
+					u = new Usuario(n, a, m, nu, c);
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return u;
+		}
+		
+		/*public static void eliminarUsuario(Connection con, String nomUsu) {
+			String sentSQL = "DELETE FROM alumno WHERE nomUsu ='"+nomUsu+"'";
+			try {
+				Statement stmt = con.createStatement();
+				stmt.executeUpdate(sentSQL);
+				stmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}*/
+
+}
---
+++ proyecto/src/proyecto/Usuario.java
@@ -0,0 +1,71 @@
+package proyecto;
+
+public class Usuario {
+	
+	private String nombre;
+	private String apellido;
+	private String mail;
+	private String nomUsuario;
+	private String contrasenia;
+
+	public Usuario() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Usuario(String nombre, String apellido, String mail, String nomUsuario, String contrasenia) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.mail = mail;
+		this.nomUsuario = nomUsuario;
+		this.contrasenia = contrasenia;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getNomUsuario() {
+		return nomUsuario;
+	}
+
+	public void setNomUsuario(String nomUsuario) {
+		this.nomUsuario = nomUsuario;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	@Override
+	public String toString() {
+		return "Nombre:" + nombre + ", apellido:" + apellido + ", mail:" + mail + ", nombre usuario:" + nomUsuario
+				+ ", contraseña=" + contrasenia;
+	}
+	
+	
+
+}
