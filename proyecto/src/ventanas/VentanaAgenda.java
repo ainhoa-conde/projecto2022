b5f36a972c466bc535c369170b4a96f695d94902
oihane.carrillo <oihane.carrillo@E105L20.opendeusto.es>
2022-11-23 12:05:04 +0100

    ventanaAgenda

--- proyecto/src/ventanas/VentanaAgenda.java
+++ proyecto/src/ventanas/VentanaAgenda.java
@@ -1,10 +1,14 @@
 package ventanas;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -32,11 +36,35 @@ public class VentanaAgenda extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 650, 560);
 		setTitle("Agenda");
-		//setIconImage(Toolkit.getDefaultToolkit().getImage("");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("imagenes/ventana_principal.png"));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setBackground(Color.WHITE);
 		
-	
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel pCentral = new JPanel();
+		contentPane.add(pCentral, BorderLayout.CENTER);
+		pCentral.setLayout(new GridLayout());
+		
+		JPanel pCentralIzq = new JPanel();
+		pCentral.add(pCentralIzq);
+		
+		JPanel pCentralDch = new JPanel();
+		pCentral.add(pCentralDch);
+		
+		JPanel pDchArriba = new JPanel();
+		pCentralDch.add(pDchArriba);
+		ImageIcon imagen = new ImageIcon("imagenes/imagenventanaInicioSesion.png");
+		JLabel pDchImagen = new JLabel(imagen);
+		pDchArriba.add(pDchImagen);
+		
+		JPanel pDchAbajo = new JPanel();
+		pCentralDch.add(pDchAbajo);
+		
+		
+		
+		
 	}
 	
 }
