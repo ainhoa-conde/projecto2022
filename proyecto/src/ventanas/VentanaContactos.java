dc490ded3f59218e1797b4c4bd8ad52f0cf1adaa
oihane.carrillo <oihane.carrillo@E105L20.opendeusto.es>
2022-11-24 12:38:55 +0100

    crear ventanaContactos

--- proyecto/src/ventanas/VentanaContactos.java
+++ proyecto/src/ventanas/VentanaContactos.java
@@ -1,5 +1,55 @@
 package ventanas;
 
-public class VentanaContactos {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+import java.sql.Connection;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import clases.BaseDatos;
+
+public class VentanaContactos extends JFrame{
+
+	private JPanel contentPane;
+	private JFrame ventanaActual, ventanaAnterior, ventanaSiguiente;
+	
+	private Connection con;
+	private BaseDatos bd;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaContactos frame = new VentanaContactos();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public VentanaContactos() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 650, 560);
+		setTitle("Contactos");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("imagenes/ventana_principal.png"));
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(Color.WHITE);
+		
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		
+		
+		
+		
+		
+	}
+	
 }
