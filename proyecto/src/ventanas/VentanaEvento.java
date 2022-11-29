d2488d6539b41131b7bc8781661dc7e4a7269f91
oicaher <oicaher@192.168.1.110>
2022-11-29 19:05:18 +0100

    añadidos los paneles, los textfiel y labels en ventanaEvento

--- proyecto/src/ventanas/VentanaEvento.java
+++ proyecto/src/ventanas/VentanaEvento.java
@@ -3,10 +3,12 @@ package ventanas;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.sql.Connection;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
@@ -16,8 +18,8 @@ import clases.BaseDatos;
 public class VentanaEvento extends JFrame{
 
 	private JPanel contentPane;
-	private JTextField txtNombreEvento;
-	private JTextField txtFechaEvento;
+	private JTextField txtNombre;
+	private JTextField txtFecha;
 	private JFrame ventanaActual, ventanaAnterior, ventanaSiguiente;
 	
 	private Connection com;
@@ -38,7 +40,7 @@ public class VentanaEvento extends JFrame{
 	
 	public VentanaEvento() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1000, 560);
+		setBounds(500, 300, 500, 200);
 		setTitle("Evento");
 		setIconImage(Toolkit.getDefaultToolkit().getImage("imagenes/ventana_principal.png"));
 		contentPane = new JPanel();
@@ -48,7 +50,33 @@ public class VentanaEvento extends JFrame{
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
+		//Panel Central
+		JPanel pCentral = new JPanel();
+		contentPane.add(pCentral, BorderLayout.CENTER);
+		pCentral.setLayout(new GridLayout(1, 2, 0, 0));
 		
+		//Panel Izq
+		JPanel pCentralIzq = new JPanel();
+		pCentral.add(pCentralIzq);
+		
+		JLabel lblNombreEvento = new JLabel("AÑADE UN TITULO: ");
+		pCentralIzq.add(lblNombreEvento);
+		
+		txtNombre = new JTextField();
+		pCentralIzq.add(txtNombre);
+		txtNombre.setColumns(10);
+		
+		
+		//Panel Drch
+		JPanel pCentralDrch = new JPanel();
+		pCentral.add(pCentralDrch);
+		
+		JLabel lblFecha = new JLabel("AÑADE UNA FECHA: ");
+		pCentralDrch.add(lblFecha);
+		
+		txtFecha = new JTextField();
+		pCentralDrch.add(txtFecha);
+		txtFecha.setColumns(10);
 		
 		
 	}
