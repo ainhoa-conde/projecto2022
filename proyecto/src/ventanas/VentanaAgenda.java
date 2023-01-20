ef801989413f608c24895b066d0c641111de9e7e
ainho <ainho@ASUS_Ainhoa>
2023-01-20 13:14:02 +0100

    corregir el isCellEditable

--- proyecto/src/ventanas/VentanaAgenda.java
+++ proyecto/src/ventanas/VentanaAgenda.java
@@ -86,6 +86,9 @@ public class VentanaAgenda extends JFrame {
 		modeloTabla = new DefaultTableModel() {
 			@Override
 			public boolean isCellEditable(int row, int column) { 
+				if(column==5) {
+					return true;
+				}
 				return false;
 			}
 		};
